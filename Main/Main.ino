0b022b1d0ee1f1a2a619ff16e41f1e2c99b68ad8
Tyler Chen <tylerc6@illinois.edu>
2019-02-02 14:29:25 -0600

    implemented controller variables via slightly janky bitwise operations

--- Main/Main.ino
+++ Main/Main.ino
@@ -12,6 +12,21 @@ XBee xbee = XBee();
 XBeeResponse response = XBeeResponse();
 Rx16Response rx16 = Rx16Response();
 
+// controller variables
+bool a = false;
+bool b = false;
+bool x = false;
+bool y = false;
+bool dup = false;
+bool ddown = false;
+bool dleft = false;
+bool dright = false;
+int leftY = 0;
+int rightY = 0;
+int ltrigger = 0;
+int rtrigger = 0;
+
+
 void failsafe() {
   // TODO: finish failsafe code
 }
@@ -31,8 +46,21 @@ void loop() {
       // received 16 bit addressed rx packet
 
       xbee.getResponse().getRx16Response(rx16);
-      // data of bit 'i' can be accessed with xbee.getData(i)
-      // TODO: process data
+
+      // set controller variables according to the packet
+      // data of bit 'i' can be accessed with rx16.getData(i)
+      a = (rx16.getData(0) & 1 != 0) ? true: false; // & 00000001
+      b = (rx16.getData(0) & 2 != 0) ? true: false; // & 00000010
+      x = (rx16.getData(0) & 4 != 0) ? true: false; // & 00000100
+      y = (rx16.getData(0) & 8 != 0) ? true: false; // & 00001000
+      dup = (rx16.getData(1) & 1 != 0) ? true: false;
+      ddown = (rx16.getData(1) & 2 != 0) ? true: false;
+      dleft = (rx16.getData(1) & 4 != 0) ? true: false;
+      dright = (rx16.getData(1) & 8 != 0) ? true: false;
+      leftY = rx16.getData(3);
+      rightY = rx16.getData(5);
+      ltrigger = rx16.getData(6);
+      rtrigger = rx16.getData(7);
     }
   }
 
