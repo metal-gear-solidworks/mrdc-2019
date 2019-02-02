7dbf6e30edd89220443300893ceb6150a20bc454
Tyler Chen <tylerc6@illinois.edu>
2019-02-01 20:03:54 -0600

    testing slack integration

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,7 +1,20 @@
 #include "lib/xbee-arduino/XBee.h"
 
+// pin constants
+
+
+// other constants
+#define BAUDRATE = 9600 // serial baud rate
+#define TIMEOUT = 50 // millisecond timeout for wireless comm
+
+// XBee setup
+XBee xbee = XBee();
+XBeeResponse response = XBeeResponse();
+Rx16Response rx16 = Rx16Response();
+
 void setup() {
-  
+  Serial.begin(BAUDRATE);
+  xbee.setSerial(Serial);
 }
 
 void loop() {
