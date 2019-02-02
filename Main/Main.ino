736333624e789bc2a21e33ee593f479cdf930cb0
Tyler Chen <tylerc6@illinois.edu>
2019-02-01 21:07:39 -0600

    finished outline of communication code

--- Main/Main.ino
+++ Main/Main.ino
@@ -12,11 +12,35 @@ XBee xbee = XBee();
 XBeeResponse response = XBeeResponse();
 Rx16Response rx16 = Rx16Response();
 
+void failsafe() {
+  // TODO: finish failsafe code
+}
+
 void setup() {
   Serial.begin(BAUDRATE);
   xbee.setSerial(Serial);
 }
 
 void loop() {
-  
+  xbee.readPacket(TIMEOUT);
+
+  if (xbee.getResponse().isAvailable()) {
+    // packet received
+    
+    if (xbee.getResponse().getApiId() == RX_16_RESPONSE) {
+      // received 16 bit addressed rx packet
+
+      xbee.getResponse().getRx16Response(rx16);
+      // data of bit 'i' can be accessed with xbee.getData(i)
+      // TODO: process data
+    }
+  }
+
+  else if (xbee.getResponse().isError()) {
+    return; // if the packet has an error, try again
+  }
+
+  else {
+    failsafe(); // if the comm times out, activate the failsafe
+  }
 }
